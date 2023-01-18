c935db4ddcecab869af6dec8148ba9bf9c8ce75c
jacek <dd@dd.pl>
2023-01-18 21:59:54 +0100

    Added items to navigation

--- src/components/navigation/MainNavigation.vue
+++ src/components/navigation/MainNavigation.vue
@@ -13,9 +13,11 @@
 
         <v-divider></v-divider>
 
-        <NavigationItemList />
+        <NavigationItemList :items="items" />
       </v-navigation-drawer>
-      <v-main style="height: 250px"> <router-view /> </v-main>
+      <v-main style="height: 250px">
+        <router-view />
+      </v-main>
     </v-layout>
   </v-card>
 </template>
@@ -26,5 +28,23 @@ import NavigationItemList from "@/components/navigation/NavigationItemList.vue";
 export default {
   name: "MainNavigation",
   components: { NavigationItemList },
+  data() {
+    return {
+      items: [
+        {
+          id: "1",
+          title: "Strona główna",
+          icon: "",
+          path: "/",
+        },
+        {
+          id: "2",
+          title: "Informacje",
+          icon: "",
+          path: "/about",
+        },
+      ],
+    };
+  },
 };
 </script>
--- src/components/navigation/NavigationItemList.vue
+++ src/components/navigation/NavigationItemList.vue
@@ -1,14 +1,23 @@
 <template>
   <v-list density="compact" nav>
-    <NavigationItem title="Strona główna" :icon="mdiAccount()" path="/" />
-    <NavigationItem title="Informacje" :icon="mdiAccount()" path="/about" />
+    <div v-for="item in items" :key="item.id">
+      <NavigationItem
+        :title="item.title"
+        :icon="mdiAccount()"
+        :path="item.path"
+      />
+    </div>
   </v-list>
 </template>
 <script>
 import NavigationItem from "@/components/navigation/NavigationItem.vue";
 import { mdiAccount } from "@mdi/js";
+
 export default {
   name: "NavigationItemList",
+  props: {
+    items: Array,
+  },
   methods: {
     mdiAccount() {
       return mdiAccount;
