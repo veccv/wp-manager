d5c1235675e0549fdb2bde22215c73418403615d
Jacek Dalmer <jacdal000@pbs.edu.pl>
2023-01-20 20:34:11 +0100

    Added connection to the backend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,23 @@
-<template>dd</template>
+<template>{{ exams.length }}--- aaa</template>
 
 <script>
 export default {
   name: "HomeView",
+  data() {
+    return {
+      exams: [],
+    };
+  },
+  methods: {
+    async fetchExams() {
+      const res = await fetch("api/exams/all");
+      const data = await res.json();
+      console.log(data);
+      return data;
+    },
+  },
+  async created() {
+    this.exams = await this.fetchExams();
+  },
 };
 </script>
