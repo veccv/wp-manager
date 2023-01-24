b2c1cb1a1db9493665e677df98cb0497be0cce77
Jacek Dalmer <jacdal000@pbs.edu.pl>
2023-01-24 18:27:32 +0100

    Added CKEditor

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import CKEditor from "@ckeditor/ckeditor5-vue";
 
 // Vuetify
 import "vuetify/styles";
@@ -14,4 +15,4 @@ const vuetify = createVuetify({
   directives,
 });
 
-createApp(App).use(vuetify).use(router).mount("#app");
+createApp(App).use(vuetify).use(router).use(CKEditor).mount("#app");
