f8265da4525d8a53b9c6763fafac8911df296e75
Jacek Dalmer <jacdal000@pbs.edu.pl>
2023-01-24 18:28:17 +0100

    Added test view

---
+++ src/views/TestView.vue
@@ -0,0 +1,19 @@
+<template>
+  <ckeditor :editor="editor" v-model="editorData" />
+</template>
+
+<script>
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
+export default {
+  name: "TestView",
+  data() {
+    return {
+      editor: ClassicEditor,
+      editorData: "<p>Content of the editor.</p>",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
